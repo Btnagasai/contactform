b378556e23cbcbdea3ba24f3399690713f259d09
Tulasi Naga Sai <btnagasai@gmail.com>
2024-10-05 22:13:05 +0530

    values

--- src/Contactform.jsx
+++ src/Contactform.jsx
@@ -3,7 +3,7 @@ const Contactform = () => {
         <div className="min-h-screen flex items-center justify-center bg-green-100  ">
       <form
       
-        className="bg-white pl-11 pr-11 pt-8  pb-10 rounded-xl shadow-lg w-full max-w-[740px] flex flex-col gap-4  "
+        className="bg-white pl-11 pr-11 pt-8  pb-10 rounded-xl shadow-lg  flex flex-col gap-4  "
       >
         <h2 className="text-[30px] font-semibold  ">Contact Us</h2>
               
@@ -12,27 +12,30 @@ const Contactform = () => {
                 <div className="flex gap-5 mt-2">
                     <div >
                     <label htmlfor="firstName" className="block mb-1 text-black text-[15px]  ">First Name   *</label>
-                    <input type="text" id="firstName" name="firstName" required className="border border-gray-300 rounded-lg w-full min-w-[315px] p-[13px] "/>
+                    <input type="text" id="firstName" name="firstName" required className="border border-gray-300 rounded-lg  p-[13px] "/>
                     </div>
                     <div>
                     <label htmlfor="" className="block text-black  mb-1 text-sm">Last Name   *</label>
-                    <input type="text" id="lastName" name="lastName" required className="border border-gray-300 rounded-lg w-full  min-w-[315px] p-[13px] "/>
+                    <input type="text" id="lastName" name="lastName" required className="border border-gray-300 rounded-lg  p-[13px] "/>
                     </div>   
                 </div>
                          {/* Email id */}
                 <div className="flex flex-col mt-3 ">
                         <label htmlfor="Email Address" className="block mb-1 text-black text-sm">Email Address  *</label>
-                        <input type="email" id="email" name="email" required className="border border-gray-300 rounded-lg w-full p-[12px]" />
+                        <input type="email" id="email" name="email" required className="border border-gray-300 rounded-lg p-[12px]" />
                 </div>
                          {/* Query Type */}
                 <div className=" mt-3">Query Type *</div>
                 <div className=" flex  gap-4 ">
-                        <div class="flex gap-4 space-x-4 border border-gray-300 rounded-lg w-full p-3 items-center">
+                    
+                    {/* General Enquiry */}
+                        
+                        <div class="flex space-x-4 border border-gray-300 rounded-lg w-full p-3 items-center">
                         <label className="flex item-center">
-                             {/* General Enquiry */}
-                        <input type="radio" name="queryType"   value="General Enquiry" className="radio-input  w-[20px] h-[20px] mr-3 ml-[11px] font-[8px] rounded-lg" />General Enquiry
+                            <input type="radio" name="queryType" value="General Enquiry" className=" radio-input  w-[20px] h-[20px] mr-3 ml-[11px] font-[8px] rounded-lg"/>General Enquiry
                         </label>
-                        </div>
+                   
+                    </div>
                             {/* Support Request */}
                         <div class="flex space-x-4 border border-gray-300 rounded-lg w-full p-3 items-center">
                         <label className="flex item-center">
@@ -44,7 +47,7 @@ const Contactform = () => {
                         {/* Message */}
                 <div className="flex flex-col mt-2">
                     <label htmlFor="Message" className="block mb-1 text-black text-sm ">Message  *</label>
-                    <textarea type="textarea" id="Message" name="Message"  rows="4" required className="border border-gray-300 rounded-lg w-full p-2 "/>
+                    <textarea type="textarea" id="Message" name="Message"  rows="4" required className="border border-gray-300 rounded-lg p-2 "/>
                 </div>
                         {/* checkbox */}
                 <div className="flex items-center mt-6 ">
