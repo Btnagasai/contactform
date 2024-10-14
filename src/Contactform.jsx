107cbec5384c0d4e16bd578acdd34ea4abd85d00
Tulasi Naga Sai <btnagasai@gmail.com>
2024-10-14 13:37:21 +0530

    querytype

--- src/Contactform.jsx
+++ src/Contactform.jsx
@@ -7,28 +7,28 @@ const Contactform = () => {
               
               {/* first & last name */}
        
-                <div className="flex flex-col sm:flex-row 2xl:flex-row gap-5 mt-2">
+                <div className="flex flex-col sm:flex-row  gap-5 mt-2">
                     <div >
                     <label htmlfor="firstName" className="block mb-1 text-black text-[15px]  ">First Name   *</label>
-                    <input type="text" id="firstName" name="firstName" required className="border border-gray-300 rounded-lg  p-[13px] "/>
+                    <input type="text" id="firstName" name="firstName" required className="border border-gray-300 rounded-lg  p-[12px] "/>
                     </div>
                     <div>
                     <label htmlfor="" className="block text-black  mb-1 text-[15px]">Last Name   *</label>
-                    <input type="text" id="lastName" name="lastName" required className="border border-gray-300 rounded-lg  p-[13px] "/>
+                    <input type="text" id="lastName" name="lastName" required className="border border-gray-300 rounded-lg  p-[12px] "/>
                     </div>   
                 </div>
                          {/* Email id */}
                 <div className="flex flex-col mt-3 ">
-                        <label htmlfor="Email Address" className="block mb-1 text-black text-sm">Email Address  *</label>
+                        <label htmlfor="Email Address" className="block mb-1 text-black text-[15px]">Email Address  *</label>
                         <input type="email" id="email" name="email" required className="border border-gray-300 rounded-lg p-[12px]" />
                 </div>
                          {/* Query Type */}
                 <div className=" mt-3">Query Type *</div>
-                <div className=" flex  gap-4 ">
+                <div className=" flex flex-col sm:flex-row gap-4 ">
                     
                     {/* General Enquiry */}
                         
-                        <div class="flex   border border-gray-300 rounded-lg w-full p-3 items-center">
+                        <div class="flex    border border-gray-300 rounded-lg w-full p-3 items-center">
                        
                             <input type="radio" name="queryType" value="General Enquiry" className=" radio-input  w-[15px] h-[15px] mr-3 ml-[11px] font-[8px] rounded-lg"/>
                             <label className="flex item-center">General Enquiry </label>
